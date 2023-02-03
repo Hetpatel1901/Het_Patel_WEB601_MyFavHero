44c1d4224fe50361f382a7b96a4bf5fe6342810c
Hetpatel1901 <W0805024@myscc.ca>
2023-02-03 18:23:05 -0500

    Content Array Created.
    Content Array Created.

--- Het_Patel_MyFavHero/src/app/content-card/content-card.component.ts
+++ Het_Patel_MyFavHero/src/app/content-card/content-card.component.ts
@@ -13,13 +13,29 @@ export class ContentCardComponent {
 
   hero1 : Content = {
     id: 0,
-    title: '',
-    description: '',
-    creator: ''
+    title: 'IronMan3',
+    description: 'Iron Man 3 is a 2013 American superhero film based on the Marvel Comics character Iron Man',
+    creator: 'Stan Lee'
+  }
+
+  hero2 : Content = {
+    id: 1,
+    title: 'Thor2',
+    description: 'Thor: The Dark World is a 2013 American superhero film based on the Marvel Comics character Thor, produced by Marvel Studios and distributed by Walt Disney.',
+    creator: 'Christopher Yost'
+  }
+
+  hero3 : Content = {
+    id: 2,
+    title: 'Captain America',
+    description: 'Recipient of the Super Soldier serum, World War II hero Steve Rogers fights for American ideals as one of the world mightiest heroes.',
+    creator: 'jack Kirby'
   }
 
   constructor(){
     this.myhero.addFunction(this.hero1);
+    this.myhero.addFunction(this.hero2);
+    this.myhero.addFunction(this.hero3);
   }
 
   displayMyIndex(index : number): string {
