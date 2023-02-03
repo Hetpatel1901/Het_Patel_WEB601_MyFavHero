c56ab012a367692586b2eae485c0351a755885fd
Hetpatel1901 <W0805024@myscc.ca>
2023-02-03 18:16:01 -0500

    content card template created for 3 valid list.
    content card template created for 3 valid list.

--- Het_Patel_MyFavHero/src/app/content-card/content-card.component.ts
+++ Het_Patel_MyFavHero/src/app/content-card/content-card.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class ContentCardComponent {
 
+  myhero : ContentList = new ContentList();
+
+  hero1 : Content = {
+    id: 0,
+    title: '',
+    description: '',
+    creator: ''
+  }
+
+  constructor(){
+    this.myhero.addFunction(this.hero1);
+  }
+
+  displayMyIndex(index : number): string {
+    return this.myhero.printIndex(index);
+}
 }
