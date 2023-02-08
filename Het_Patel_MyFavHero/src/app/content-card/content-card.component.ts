472854d8f23185a42850a3892b0e222efa35b126
Hetpatel1901 <W0805024@myscc.ca>
2023-02-07 21:20:56 -0500

    content-list added and working on css.
    content-list added and working on css.

--- Het_Patel_MyFavHero/src/app/content-card/content-card.component.ts
+++ Het_Patel_MyFavHero/src/app/content-card/content-card.component.ts
@@ -1,44 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
-import { ContentList } from '../helper-files/content-list';
 
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
   styleUrls: ['./content-card.component.scss']
 })
-export class ContentCardComponent {
-
-  myhero : ContentList = new ContentList();
-
-  hero1 : Content = {
-    id: 0,
-    title: 'IronMan3',
-    description: 'Iron Man 3 is a 2013 American superhero film based on the Marvel Comics character Iron Man',
-    creator: 'Stan Lee'
+export class ContentCardComponent implements OnInit {
+ 
+  @Input() content?: Content;
+  private _item: Content[];
+  constructor() {
+    this._item=[];
   }
 
-  hero2 : Content = {
-    id: 1,
-    title: 'Thor2',
-    description: 'Thor: The Dark World is a 2013 American superhero film based on the Marvel Comics character Thor, produced by Marvel Studios and distributed by Walt Disney.',
-    creator: 'Christopher Yost'
+  getItem(): Content[] {
+    return this._item;
   }
 
-  hero3 : Content = {
-    id: 2,
-    title: 'Captain America',
-    description: 'Recipient of the Super Soldier serum, World War II hero Steve Rogers fights for American ideals as one of the world mightiest heroes.',
-    creator: 'jack Kirby'
+  pritnItem() {
+    console.log(this.content?.id);
+    console.log(this.content?.title);
+    console.log(this.content?.description);
+    console.log(this.content?.creator);
   }
 
-  constructor(){
-    this.myhero.addFunction(this.hero1);
-    this.myhero.addFunction(this.hero2);
-    this.myhero.addFunction(this.hero3);
-  }
 
-  displayMyIndex(index : number): string {
-    return this.myhero.printIndex(index);
-}
+  ngOnInit(): void {
+  }
+  
 }
