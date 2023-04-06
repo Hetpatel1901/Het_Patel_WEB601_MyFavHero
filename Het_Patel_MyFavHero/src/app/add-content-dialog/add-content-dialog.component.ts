1f7e14e29bda88f16697a67abe3f0ebdbd283808
Hetpatel1901 <W0805024@myscc.ca>
2023-04-06 17:50:11 -0400

    Content dialog component added.
    Content dialog component added.

--- Het_Patel_MyFavHero/src/app/add-content-dialog/add-content-dialog.component.ts
+++ Het_Patel_MyFavHero/src/app/add-content-dialog/add-content-dialog.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ModifyContentComponent } from '../modify-content/modify-content.component';
+
+
 @Component({
   selector: 'app-add-content-dialog',
   templateUrl: './add-content-dialog.component.html',
   styleUrls: ['./add-content-dialog.component.scss']
 })
 export class AddContentDialogComponent {
+    dialogTitle = 'Add HeroType';
 
+    constructor(
+      public dialogRef: MatDialogRef<ModifyContentComponent>,
+      @Inject(MAT_DIALOG_DATA) public data: any
+    ) {}
+  
+    onCancelClick(): void {
+      this.dialogRef.close();
+    }
+    
+    onAddClick(): void {
+      this.dialogRef.close(this.data);
+    }  
 }
