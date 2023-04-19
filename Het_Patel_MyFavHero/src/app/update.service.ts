919b581d754d21e4e1975902fbf045d07688af6a
Hetpatel1901 <W0805024@myscc.ca>
2023-04-19 18:12:24 -0400

    Update.service.ts updated to showcase snackbar

--- Het_Patel_MyFavHero/src/app/update.service.ts
+++ Het_Patel_MyFavHero/src/app/update.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateService {
+  constructor(private swUpdate: SwUpdate) {
+    if (swUpdate.isEnabled) {
+      swUpdate.available.subscribe(() => {
+        // Show the snackbar here
+      });
+    }
+  }
 
-  constructor() { }
+  activateUpdate() {
+    this.swUpdate.activateUpdate().then(() => document.location.reload());
+  }
 }
