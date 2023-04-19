ba196a806f25aad1e910d6724f0ec7a28744cee8
Hetpatel1901 <W0805024@myscc.ca>
2023-04-19 18:14:27 -0400

    app.components.ts updated to Integrate the service

--- Het_Patel_MyFavHero/src/app/app.component.ts
+++ Het_Patel_MyFavHero/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UpdateService } from './update.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Het_Patel_MyFavHero';
+  constructor(private updateService: UpdateService, private snackBar: MatSnackBar) {
+    this.updateService.swUpdate.available.subscribe(() => {
+      const snackBarRef = this.snackBar.open('Update available', 'Update', { duration: 5000 });
+  
+      snackBarRef.onAction().subscribe(() => {
+        this.updateService.activateUpdate();
+      });
+    });
+  }
 }
+
